c09e20808588d7b4eecedb5ad94e9defb9ec350b
Aaron Ross <ashmortar@gmail.com>
2023-12-01 18:32:16 -0800

    Add bench

---
+++ 2023/day-1/main_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	_ "embed"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+//go:embed puzzle.txt
+var testInput string
+
+func TestPart1(t *testing.T) {
+	input := strings.Split(testInput, "\n")
+	assert.Equal(t, 54605, part1(input))
+}
+
+func BenchmarkPart1(b *testing.B) {
+	input := strings.Split(testInput, "\n")
+	for n := 0; n < b.N; n++ {
+		part1(input)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	input := strings.Split(testInput, "\n")
+	assert.Equal(t, 55429, part2(input))
+}
+
+func BenchmarkPart2(b *testing.B) {
+	input := strings.Split(testInput, "\n")
+	for n := 0; n < b.N; n++ {
+		part2(input)
+	}
+}
+
+func TestPart2Replace(t *testing.T) {
+	input := strings.Split(testInput, "\n")
+	assert.Equal(t, 55429, part2Replace(input))
+}
+
+func BenchmarkPart2Replace(b *testing.B) {
+	input := strings.Split(testInput, "\n")
+	for n := 0; n < b.N; n++ {
+		part2Replace(input)
+	}
+}
